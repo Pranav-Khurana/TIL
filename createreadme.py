d00121b1ed5d6f590b2707999bceb3e5ba8f4efe
Pranav Khurana <apranavkhurana18@gmail.com>
2019-06-19 21:13:25 +0530

    updated createreadme.py file

--- createreadme.py
+++ createreadme.py
@@ -4,10 +4,11 @@ HEADER = '''# T I L
 > Today-I-Learned Repository
 
 This is my 'Today-I-Learned' repository which is used to keep a record of whatever I learn in daily basis. 
+
 '''
 
 FOOTER = '''## Details
-Want to know more about TIL repositories, see [Details.md]( ADD LINK OF Details.md file) 
+Want to know more about TIL repositories, see [Details.md](https://github.com/Pranav-Khurana/TIL/blob/master/Details.md) 
 '''
 
 
